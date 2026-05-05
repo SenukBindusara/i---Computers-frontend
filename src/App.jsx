80bbcf3dc6a3e18e19fcd5b3ab79b0e17c1c3875
SenukBindusara <nayanaja200615@gmail.com>
2026-05-05 10:04:41 +0530

    inzalitions

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProductCart from "./components/productCart"
+
+function App() {
+
+  return (
+    <div className="bg-amber-800 border-rose-100">
+      <h1>Hello World</h1>
+      <ProductCart> </ProductCart>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/productCart.jsx
@@ -0,0 +1,10 @@
+export default function ProductCart(){
+    return(
+        <div>
+            <h2>Macbook</h2>
+            <img src="https://picsum.photos/id/0/200/300"  />
+            <p>Price LKR230,000.00</p>
+            <button>Buy Now</button>
+        </div>
+    )
+}
